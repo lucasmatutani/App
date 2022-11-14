c42259f26b28f662f263d59577f08f5f5f79d6ae
lucasmatutani <lucasmatutani@gmail.com>
2022-11-14 13:06:34 -0300

    hotfix: adicionando páginas e arquivos de conexão

---
+++ teste-comportamental/teste-comportamental.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../connection.php";
+
+$nome = $_REQUEST['nome'];
+$empresa = $_REQUEST['empresa'];
+$email = $_REQUEST['email'];
+$aguia = $_REQUEST['aguia'];
+$gato = $_REQUEST['gato'];
+$tubarao = $_REQUEST['tubarao'];
+$lobo = $_REQUEST['lobo'];
+
+$sql = "INSERT INTO teste_comportamental1 (nome, empresa, email, aguia, gato, tubarao, lobo) VALUES ('$nome', '$empresa', '$email', $aguia, $gato, $tubarao, $lobo)";
+if (mysqli_query($conn, $sql)) {
+    echo "RESPOSTA GRAVADA COM SUCESSO!";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
