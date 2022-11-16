2d2b8bd85ca6c5934b72995c363b502fdd2af1d4
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-11-16 11:42:59 -0300

    adicionando placeholder

--- cadastro/index.php
+++ cadastro/index.php
@@ -23,28 +23,27 @@
     <section class="container-cadastro">
         <div class="card">
             <div class="container-nome">
-                <label for="nome">Nome Completo</label>
-                <input type="text" name="nome">
+                <input type="text" name="nome" placeholder="Nome Completo">
             </div>
 
             <div class="container-empresa">
-                <label for="empresa">Empresa</label>
-                <input type="text" name="empresa">
+
+                <input type="text" name="empresa" placeholder="Empresa">
             </div>
 
             <div class="container-email">
-                <label for="email">E-mail</label>
-                <input type="text" name="email">
+
+                <input type="text" name="email" placeholder="E-mail">
             </div>
 
             <div class="container-senha">
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" id="">
+
+                <input type="password" name="senha" id="" placeholder="Senha">
             </div>
             <div class="confirme-senha">
 
-                <label for="confirmeSenha">Confirme sua senha</label>
-                <input type="password" name="confirmeSenha" id="">
+
+                <input type="password" name="confirmeSenha" id="" placeholder="Confirme sua senha">
             </div>
             <button class="button"><span>Enviar </span></button>
         </div>
