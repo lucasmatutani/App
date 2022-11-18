2d7828acdeeccb27adfebcfbe6ae523ff09bd873
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-11-18 13:55:55 -0300

    criação da página de admin

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/style.css">
+    <title>Administrador</title>
+</head>
+
+<body>
+    <div class="img-logo">
+        <img src="../assets/img/logo-focustrade.jpeg" alt="">
+    </div>
+
+    <div class="container-card">
+
+        <a href="../cadastro/" class="btn-3d" title="Cadastro de usuário">
+            <i class="icon icon-cube"> Cadastro de usuário</i>
+        </a>
+
+        <div class="container-Banco">
+            <a href="https://mysql11-farm1.kinghost.net/login.php?phpMyAdmin=25d443d67ebbd8774f7cbc5e5f9600bd0774d66d" class="btn-3d2" title="Banco de Dados">
+                <i class="icon icon-cube"> Banco de Dados</i>
+            </a>
+        </div>
+    </div>
+</body>
+
+</html>
