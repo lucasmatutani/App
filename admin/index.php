c44768d1b871824ce914709a6a637ec97689795f
lucasmatutani <lucasmatutani@gmail.com>
2022-11-21 19:12:55 -0300

    hotffix: botões adicionados

--- admin/index.php
+++ admin/index.php
@@ -24,6 +24,14 @@
             <i class="icon icon-cube">Testes</i>
         </a>
 
+        <a href="#" class="btn-3d" title="Cadastro de usuário" target="_blank">
+            <i class="icon icon-cube">Resposta Teste AIDA</i>
+        </a>
+
+        <a href="#" class="btn-3d" title="Cadastro de usuário" target="_blank">
+            <i class="icon icon-cube">Resposta Teste Spin</i>
+        </a>
+
         <div class="container-Banco">
             <a href="https://mysql11-farm1.kinghost.net/login.php?phpMyAdmin=25d443d67ebbd8774f7cbc5e5f9600bd0774d66d" target="_blank" class="btn-3d" title="Banco de Dados">
                 <i class="icon icon-cube"> Banco de Dados</i>
