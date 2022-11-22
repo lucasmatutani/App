8e3a88a26ddc1264b443a58af29a8291e6751409
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-11-22 15:59:34 -0300

    página rolePlay

---
+++ teste-aida/resposta.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./resposta.css">
+    <title>Respotas A I D A</title>
+</head>
+
+<body>
+    <div class="container-titulo">
+
+        <img class="img-logo" src="../assets/img/logo-focustrade.jpeg" alt="">
+
+        <img class="img-titulo" src="../assets/img/logo-aida-sem-fundo.png" alt="">
+    </div>
+
+    <h3 class="h-titulo">Meios de comunicação</h3>
+
+    <form action="">
+        <div class="container-table">
+            <table cellpadding="30">
+                <tr style="text-align: left;">
+
+                    <th class="th-nm1" style="width: 100%;">Focus Trade</th>
+
+                    <th style="font-size: 1em;"> <a class="btn-editar" href="">editar</a> </th>
+                    <th class="th-nmr1">10</th>
+                </tr>
+
+            </table>
+        </div>
+    </form>
+</body>
+
+</html>
