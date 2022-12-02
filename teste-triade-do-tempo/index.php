f8aadcd9227b9150601d4dd6ae03dbcebe0d7f15
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-12-02 11:26:11 -0300

    criação teste triade do tempo

---
+++ teste-triade-do-tempo/index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Tríade do Tempo</title>
+</head>
+
+<body>
+    <div class="container-header">
+        <img class="img-logo" src="../assets/img/logo-focustrade.jpeg" alt="">
+        <h1>Teste - Tríade do Tempo</h1>
+    </div>
+    <div class="container-subtitulo">
+        <p>1 Nunca</p>
+        <p>2 Raramente</p>
+        <p>3 Às vezes</p>
+        <p>4 Quase sempre</p>
+        <p>5 Sempre</p>
+    </div>
+    <form action="">
+
+        <div class="container-pergunta">
+            <div>
+                <p>1 - Costumo ir a eventos, mesmo sem ter muita vontade, para <br> agradar meu chefe, meus amigos ou minha familia.</p>
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio1" id="">
+                        <input type="radio" name="radio1" id="">
+                        <input type="radio" name="radio1" id="">
+                        <input type="radio" name="radio1" id="">
+                        <input type="radio" name="radio1" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+                <p>2 - Não consigo realizar tudo que me propus a fazer no dia; preciso <br>fazer hora extra e levar trabalho para casa.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio2" id="">
+                        <input type="radio" name="radio2" id="">
+                        <input type="radio" name="radio2" id="">
+                        <input type="radio" name="radio2" id="">
+                        <input type="radio" name="radio2" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>3 - Quando recebo um novo e-mail, costumo dar uma olhada para<br> checar o conteúdo.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio3" id="">
+                        <input type="radio" name="radio3" id="">
+                        <input type="radio" name="radio3" id="">
+                        <input type="radio" name="radio3" id="">
+                        <input type="radio" name="radio3" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>4 - Costumo visitar, com regularidade, pessoas relevantes em<br> minha vida, como amigos, parentes e filhos.
+                <p>
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio4" id="">
+                        <input type="radio" name="radio4" id="">
+                        <input type="radio" name="radio4" id="">
+                        <input type="radio" name="radio4" id="">
+                        <input type="radio" name="radio4" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>5 - É comum aparecerem problemas inesperados no meu dia a dia. </p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio5" id="">
+                        <input type="radio" name="radio5" id="">
+                        <input type="radio" name="radio5" id="">
+                        <input type="radio" name="radio5" id="">
+                        <input type="radio" name="radio5" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>6 - Assumo compromissos com outras pessoas ou aceito novas posições<br> na empresa, mesmo que não goste muito da nova atividade, se for<br> para aumentar meus rendimentos ou obter uma proporção.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio6" id="">
+                        <input type="radio" name="radio6" id="">
+                        <input type="radio" name="radio6" id="">
+                        <input type="radio" name="radio6" id="">
+                        <input type="radio" name="radio6" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>7 Tenho um tempo para dedicar a mim mesmo, e nele posso fazer o <br>que quiser.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio7" id="">
+                        <input type="radio" name="radio7" id="">
+                        <input type="radio" name="radio7" id="">
+                        <input type="radio" name="radio7" id="">
+                        <input type="radio" name="radio7" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>8 Costumo deixar para fazer relatórios, imposto de renda, compras<br> de Natal, estudar para provas e outras tarefas perto do prazo de entrega.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio8" id="">
+                        <input type="radio" name="radio8" id="">
+                        <input type="radio" name="radio8" id="">
+                        <input type="radio" name="radio8" id="">
+                        <input type="radio" name="radio8" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>9 - Nos dias de descanso, costumo passar boa parte do dia assistindo à <br> televisão, jogando ou acessando a internet. </p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio9" id="">
+                        <input type="radio" name="radio9" id="">
+                        <input type="radio" name="radio9" id="">
+                        <input type="radio" name="radio9" id="">
+                        <input type="radio" name="radio9" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+                <p>10 Faço um planejamento por escrito de tudo que preciso fazer durante<br> minha semana.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio10" id="">
+                        <input type="radio" name="radio10" id="">
+                        <input type="radio" name="radio10" id="">
+                        <input type="radio" name="radio10" id="">
+                        <input type="radio" name="radio10" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>11 Posso afirmar que estou conseguindo realizar tudo que gostaria em<br> minha vida e que o tempo está passando na realidade correta.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio11" id="">
+                        <input type="radio" name="radio11" id="">
+                        <input type="radio" name="radio11" id="">
+                        <input type="radio" name="radio11" id="">
+                        <input type="radio" name="radio11" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>12 Costumo participar de reuniões sem saber direito o conteúdo, por que<br> devo participar ou a que resultado aquele encontro pode levar.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio12" id="">
+                        <input type="radio" name="radio12" id="">
+                        <input type="radio" name="radio12" id="">
+                        <input type="radio" name="radio12" id="">
+                        <input type="radio" name="radio12" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>13 Consigo melhores resultados e me sinto mais produtivo quando estou sob<br> pressão ou com o prazo curto.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio13" id="">
+                        <input type="radio" name="radio13" id="">
+                        <input type="radio" name="radio13" id="">
+                        <input type="radio" name="radio13" id="">
+                        <input type="radio" name="radio13" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>14 Quando quero alguma coisa, defino esse objetivo por escrito, estabeleço<br> prazo em minha agenda, monitoro os resultados obtidos e os comparo com<br> os esperados.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio14" id="">
+                        <input type="radio" name="radio14" id="">
+                        <input type="radio" name="radio14" id="">
+                        <input type="radio" name="radio14" id="">
+                        <input type="radio" name="radio14" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>15 Leio muitos e-mails desnecessários, com piadas, correntes, propagandas,<br> apresentações, produtos e etc.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio15" id="">
+                        <input type="radio" name="radio15" id="">
+                        <input type="radio" name="radio15" id="">
+                        <input type="radio" name="radio15" id="">
+                        <input type="radio" name="radio15" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>16 Estive atrasado com minhas tarefas ou reuniões nas últimas semanas.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio16" id="">
+                        <input type="radio" name="radio16" id="">
+                        <input type="radio" name="radio16" id="">
+                        <input type="radio" name="radio16" id="">
+                        <input type="radio" name="radio16" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>17 Faço esporte com regularidade, me alimentode forma adequada e tenho o<br> lazer que gostaria.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio17" id="">
+                        <input type="radio" name="radio17" id="">
+                        <input type="radio" name="radio17" id="">
+                        <input type="radio" name="radio17" id="">
+                        <input type="radio" name="radio17" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+
+                <p>18 É comum reduzir meu hórario de almoço ou até mesmo comer enquanto <br>trabalho para concluir um projeto um projeto ou tarefa.</p>
+
+                <div class="container-radios">
+                    <div class="container-input">
+                        <input type="radio" name="radio18" id="">
+                        <input type="radio" name="radio18" id="">
+                        <input type="radio" name="radio18" id="">
+                        <input type="radio" name="radio18" id="">
+                        <input type="radio" name="radio18" id="">
+                    </div>
+                    <div class="container-label">
+                        <label for="">1</label>
+                        <label for="">2</label>
+                        <label for="">3</label>
+                        <label for="">4</label>
+                        <label for="">5</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
