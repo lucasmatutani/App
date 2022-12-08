4f9c832d114099b03f2e8108cd429a887540e8d2
lucasmatutani <lucasmatutani@gmail.com>
2022-12-08 14:43:54 -0300

    hotfix

--- teste-metodo-cesar/index.php
+++ teste-metodo-cesar/index.php
@@ -1,3 +1,10 @@
+<?php
+include_once "../includes/connection.php";
+session_start();
+$usuario_id = $_SESSION['usuarioId'];
+
+$query = "SELECT * FROM teste_metodo_cesar WHERE usuario_id = $usuario_id";
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,44 +20,93 @@
         <img src="../assets/img/logo-focustrade.jpeg" alt="logo" style="min-width: 300px;">
         <h1>MÉTODO CESAR</h1>
     </div>
-    <form action="./sql.php" method="POST">
-        <div class="container-case">
-            <h2>Escolha um case de atendimento do dia a dia</h2>
-            <textarea name="case" id="" cols="30" rows="10" required></textarea>
-        </div>
+    <?php
+    $data = $conn->query("SELECT * FROM teste_metodo_cesar WHERE usuario_id = $usuario_id");
+    $linha = mysqli_fetch_assoc($data);
+    if ($linha['usuario_id'] == $usuario_id) {
+        if ($res = mysqli_query($conn, $query))
+            while ($row = $res->fetch_array(MYSQLI_ASSOC)) : ?>
+            <form action="./sql.php" method="POST">
+                <input type="hidden" name="usuario_id" value="<?php echo $_SESSION['usuarioId'] ?>">
 
-        <h1 class="atitudes">Atitudes</h1>
+                <div class="container-case">
+                    <h2>Escolha um case de atendimento do dia a dia</h2>
+                    <textarea name="case" id="" cols="30" rows="10" required><?php echo $row['caso'] ?></textarea>
+                </div>
 
-        <div class="container-atitudes">
-            <div class="container-resposta">
-                <h2>Confiança</h2>
-                <textarea name="confianca" id="" cols="30" rows="10" required></textarea>
-            </div>
-            <div class="container-resposta">
-                <h2>Empatia</h2>
-                <textarea name="empatia" id="" cols=" 30" rows="10" required></textarea>
-            </div>
-            <div class="container-resposta">
-                <h2>Segurança</h2>
-                <textarea name="seguranca" id="" cols="30" rows="10" required></textarea>
-            </div>
-            <div class="container-resposta">
-                <h2>Aparência</h2>
-                <textarea name="aparencia" id="" cols="30" rows="10" required></textarea>
-            </div>
-            <div class="container-resposta">
-                <h2>Resposta</h2>
-                <textarea name="resposta" id="" cols=" 30" rows="10" required></textarea>
-            </div>
-        </div>
+                <h1 class="atitudes">Atitudes</h1>
 
-        <div class="container-observacoes">
-            <h1>Observações</h1>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
+                <div class="container-atitudes">
+                    <div class="container-resposta">
+                        <h2>Confiança</h2>
+                        <textarea name="confianca" id="" cols="30" rows="10" required><?php echo $row['confianca'] ?></textarea>
+                    </div>
+                    <div class="container-resposta">
+                        <h2>Empatia</h2>
+                        <textarea name="empatia" id="" cols=" 30" rows="10" required><?php echo $row['empatia'] ?></textarea>
+                    </div>
+                    <div class="container-resposta">
+                        <h2>Segurança</h2>
+                        <textarea name="seguranca" id="" cols="30" rows="10" required><?php echo $row['seguranca'] ?></textarea>
+                    </div>
+                    <div class="container-resposta">
+                        <h2>Aparência</h2>
+                        <textarea name="aparencia" id="" cols="30" rows="10" required><?php echo $row['aparencia'] ?></textarea>
+                    </div>
+                    <div class="container-resposta">
+                        <h2>Resposta</h2>
+                        <textarea name="resposta" id="" cols=" 30" rows="10" required><?php echo $row['resposta'] ?></textarea>
+                    </div>
+                </div>
 
-        <input id="botao-enviar" type="submit" value="ENVIAR">
-    </form>
+                <div class="container-observacoes">
+                    <h1>Observações</h1>
+                    <textarea name="observacoes" id="" cols="30" rows="10"><?php if (!empty($row['observacoes'])) echo $row['observacoes'] ?></textarea>
+                </div>
+            <?php endwhile; ?>
+            <input id="botao-enviar" type="submit" value="ENVIAR">
+            </form>
+        <?php } ?>
+
+        <form action="./sql.php" method="POST">
+            <input type="hidden" name="usuario_id" value="<?php echo $_SESSION['usuarioId'] ?>">
+
+            <div class="container-case">
+                <h2>Escolha um case de atendimento do dia a dia</h2>
+                <textarea name="case" id="" cols="30" rows="10" required></textarea>
+            </div>
+
+            <h1 class="atitudes">Atitudes</h1>
+
+            <div class="container-atitudes">
+                <div class="container-resposta">
+                    <h2>Confiança</h2>
+                    <textarea name="confianca" id="" cols="30" rows="10" required></textarea>
+                </div>
+                <div class="container-resposta">
+                    <h2>Empatia</h2>
+                    <textarea name="empatia" id="" cols=" 30" rows="10" required></textarea>
+                </div>
+                <div class="container-resposta">
+                    <h2>Segurança</h2>
+                    <textarea name="seguranca" id="" cols="30" rows="10" required></textarea>
+                </div>
+                <div class="container-resposta">
+                    <h2>Aparência</h2>
+                    <textarea name="aparencia" id="" cols="30" rows="10" required></textarea>
+                </div>
+                <div class="container-resposta">
+                    <h2>Resposta</h2>
+                    <textarea name="resposta" id="" cols=" 30" rows="10" required></textarea>
+                </div>
+            </div>
+
+            <div class="container-observacoes">
+                <h1>Observações</h1>
+                <textarea name="observacoes" id="" cols="30" rows="10"></textarea>
+            </div>
+            <input id="botao-enviar" type="submit" value="ENVIAR">
+        </form>
 </body>
 
 </html>
--- teste-metodo-cesar/sql.php
+++ teste-metodo-cesar/sql.php
@@ -1,14 +1,22 @@
 <?php
 include_once "../includes/connection.php";
 
+$usuario_id = $_REQUEST['usuario_id'];
 $case = $_REQUEST['case'];
 $confianca = $_REQUEST['confianca'];
 $empatia = $_REQUEST['empatia'];
 $seguranca = $_REQUEST['seguranca'];
 $aparencia = $_REQUEST['aparencia'];
 $resposta = $_REQUEST['resposta'];
+$observacoes = $_REQUEST['observacoes'];
 
-$sql = "INSERT INTO teste_metodo_cesar (caso, confianca, empatia, seguranca, aparencia, resposta) VALUES ('$case', '$confianca', '$empatia', '$seguranca', '$aparencia', '$resposta')";
+$data = $conn->query('SELECT * FROM teste_metodo_cesar');
+$linha = mysqli_fetch_assoc($data);
+if ($linha['usuario_id'] == $usuario_id) {
+    $sql = "UPDATE teste_metodo_cesar set caso='$case', confianca='$confianca', empatia='$empatia', seguranca='$seguranca', aparencia='$aparencia', resposta='$resposta', observacoes='$observacoes' WHERE usuario_id= $usuario_id";
+} else {
+    $sql = "INSERT INTO teste_metodo_cesar (caso, confianca, empatia, seguranca, aparencia, resposta, observacoes, usuario_id) VALUES ('$case', '$confianca', '$empatia', '$seguranca', '$aparencia', '$resposta', '$observacoes' , '$usuario_id')";
+}
 
 if (mysqli_query($conn, $sql)) {
     echo "RESPOSTA GRAVADA COM SUCESSO!";
