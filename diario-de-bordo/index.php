b87ebfb8382eccdced9e6211f9926ca29ec82c55
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-12-08 16:29:57 -0300

    criação diario de bordo

---
+++ diario-de-bordo/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Diário de Bordo</title>
+</head>
+
+<body>
+    <div class="container-header">
+        <img src="../assets/img/logo-focustrade.jpeg" alt="">
+        <h1>Diário de Bordo</h1>
+    </div>
+
+    <form action="">
+        <div class="container-perguntas">
+            <p>1. Por que valeu a pena trabalhar e liderar no dia de hoje? </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>1.1. Que ideia você pode ter hoje para contribuir para a construção de um ambiente melhor de se viver, trabalhar e servir no tempo presente?</p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p> Como você pode colocar isso em prática? </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>2. Se você pudesse voltar no tempo e tivesse o poder de modificar algum acontecimento no dia de hoje, o que você faria de diferente? </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>3. 6 ações que você se compromete a realizar no dia de amanhã: </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>3. 6 ações que você se compromete a realizar no dia de amanhã: </p>
+            <h6>1<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+            <h6>2<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+            <h6>3<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+            <h6>4<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+            <h6>5<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+            <h6>6<textarea name="" id="" cols="20" rows="1"></textarea></h6>
+
+            <p>3.1. Numa escala de 0 a 10, quanto você acredita que essas ações contribuirão para um dia produtivo amanhã? </p>
+            <div class="container-radios">
+                <div class="container-input">
+                    <input type="radio" name="radio1" value="radio1" id="">
+                    <input type="radio" name="radio2" value="radio2" id="">
+                    <input type="radio" name="radio3" value="radio3" id="">
+                    <input type="radio" name="radio4" value="radio4" id="">
+                    <input type="radio" name="radio5" value="radio5" id="">
+                    <input type="radio" name="radio6" value="radio6" id="">
+                    <input type="radio" name="radio7" value="radio7" id="">
+                    <input type="radio" name="radio8" value="radio8" id="">
+                    <input type="radio" name="radio9" value="radio9" id="">
+                    <input type="radio" name="radio10" value="radio10" id="">
+                </div>
+
+                <div class="container-label">
+                    <label for="">1</label>
+                    <label for="">2</label>
+                    <label for="">3</label>
+                    <label for="">4</label>
+                    <label for="">5</label>
+                    <label for="">6</label>
+                    <label for="">7</label>
+                    <label for="">8</label>
+                    <label for="">9</label>
+                    <label for="">10</label>
+                </div>
+            </div>
+
+            <p> Com quais argumentos você confirma sua nota? </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+
+            <p>4. GRATIDÃO: agradeça por 3 acontecimentos, emoções ou fatos positivos, sentimentos ou conquistas do dia de hoje.</p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>5.1. Quais foram os aprendizados que você teve ao fazer o seu diário de bordo no dia de hoje? </p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+            <p>Considerando o papel de líder, o que você conclui do dia de hoje?</p>
+            <textarea name="" id="" cols="50" rows="10"></textarea>
+
+        </div>
+    </form>
+
+</body>
+
+</html>
