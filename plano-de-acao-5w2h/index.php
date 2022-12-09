a273b0657c447912f8c914cf6547da601be90406
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2022-12-08 21:46:59 -0300

    criação banco de dados 5wh2

--- plano-de-acao-5w2h/index.php
+++ plano-de-acao-5w2h/index.php
@@ -1,3 +1,13 @@
+<?php
+include_once "../includes/connection.php";
+session_start();
+$usuario_id = $_SESSION['usuarioId'];
+
+$data = $conn->query("SELECT * FROM plano_de_acao_5w2h WHERE usuario_id = $usuario_id");
+if (!empty($data)) {
+    $linha = mysqli_fetch_assoc($data);
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,20 +23,21 @@
     <div class="container-header">
         <img src="../assets/img/logo-focustrade.jpeg" alt="">
         <h1>Plano de ação 5W2H</h1>
-        <p>Objetivo:</p>
-        <input type="text" name="" id="">
     </div>
 
-    <form action="">
+    <form action="./sql.php" method="POST">
+        <input type="hidden" name="usuario_id" value="<?php echo $_SESSION['usuarioId'] ?>">
 
         <div class="container-titulo">
             <div class="container-1">
-                <p>Consultor:</p> <input type="text">
-                <p>Setor:</p> <input type="text">
+                <p>Objetivo:</p>
+                <input type="text" name="objetivo" value="<?php if (!empty($linha)) echo $linha['objetivo']; ?>">
+                <p>Consultor:</p> <input type="text" name="consultor" value="<?php if (!empty($linha)) echo $linha['consultor']; ?>">
+                <p>Setor:</p> <input type="text" name="setor" value="<?php if (!empty($linha)) echo $linha['setor']; ?>">
             </div>
             <div class="container-2">
-                <p>Data de Elaboração:</p> <input type="text">
-                <p>Última Atualização:</p> <input type="text">
+                <p>Data de Elaboração:</p> <input type="text" name="elaboracao" value="<?php if (!empty($linha)) echo $linha['elaboracao']; ?>">
+                <p>Última Atualização:</p> <input type="text" name="atualizacao" value="<?php if (!empty($linha)) echo $linha['atualizacao']; ?>">
             </div>
         </div>
 
@@ -64,78 +75,78 @@
                 <div>100</div>
             </div>
             <div></div>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
+            <textarea name="txt1" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt1']; ?></textarea>
+            <textarea name="txt2" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt2']; ?></textarea>
+            <textarea name="txt3" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt3']; ?></textarea>
+            <textarea name="txt4" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt4']; ?></textarea>
+            <textarea name="txt5" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt5']; ?></textarea>
+            <textarea name="txt6" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt6']; ?></textarea>
+            <textarea name="txt7" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt7']; ?></textarea>
+            <textarea name="txt8" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt8']; ?></textarea>
+            <textarea name="txt9" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt9']; ?></textarea>
+            <textarea name="txt10" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt10']; ?></textarea>
             <div class="subgrid">
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
+                <textarea name="txt11" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt11']; ?></textarea>
+                <textarea name="txt12" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt12']; ?></textarea>
+                <textarea name="txt13" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt13']; ?></textarea>
+                <textarea name="txt14" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt14']; ?></textarea>
+                <textarea name="txt15" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt15']; ?></textarea>
             </div>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
+            <textarea name="txt16" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt16']; ?></textarea>
+            <textarea name="txt17" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt17']; ?></textarea>
+            <textarea name="txt18" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt18']; ?></textarea>
+            <textarea name="txt19" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt19']; ?></textarea>
+            <textarea name="txt20" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt20']; ?></textarea>
+            <textarea name="txt21" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt21']; ?></textarea>
+            <textarea name="txt22" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt22']; ?></textarea>
+            <textarea name="txt23" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt23']; ?></textarea>
+            <textarea name="txt24" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt24']; ?></textarea>
+            <textarea name="txt25" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt25']; ?></textarea>
+            <textarea name="txt26" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt26']; ?></textarea>
             <div class="subgrid">
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
+                <textarea name="txt27" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt27']; ?></textarea>
+                <textarea name="txt28" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt28']; ?></textarea>
+                <textarea name="txt29" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt29']; ?></textarea>
+                <textarea name="txt30" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt30']; ?></textarea>
+                <textarea name="txt31" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt31']; ?></textarea>
             </div>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
+            <textarea name="txt32" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt32']; ?></textarea>
+            <textarea name="txt33" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt33']; ?></textarea>
+            <textarea name="txt34" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt34']; ?></textarea>
+            <textarea name="txt35" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt35']; ?></textarea>
+            <textarea name="txt36" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt36']; ?></textarea>
+            <textarea name="txt37" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt37']; ?></textarea>
+            <textarea name="txt38" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt38']; ?></textarea>
+            <textarea name="txt39" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt39']; ?></textarea>
+            <textarea name="txt40" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt40']; ?></textarea>
+            <textarea name="txt41" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt41']; ?></textarea>
+            <textarea name="txt42" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt42']; ?></textarea>
             <div class="subgrid">
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
+                <textarea name="txt43" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt43']; ?></textarea>
+                <textarea name="txt44" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt44']; ?></textarea>
+                <textarea name="txt45" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt45']; ?></textarea>
+                <textarea name="txt46" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt46']; ?></textarea>
+                <textarea name="txt47" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt47']; ?></textarea>
             </div>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
+            <textarea name="txt48" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt48']; ?></textarea>
+            <textarea name="txt49" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt49']; ?></textarea>
+            <textarea name="txt50" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt50']; ?></textarea>
+            <textarea name="txt51" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt51']; ?></textarea>
+            <textarea name="txt52" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt52']; ?></textarea>
+            <textarea name="txt53" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt53']; ?></textarea>
+            <textarea name="txt54" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt54']; ?></textarea>
+            <textarea name="txt55" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt55']; ?></textarea>
+            <textarea name="txt56" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt56']; ?></textarea>
+            <textarea name="txt57" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt57']; ?></textarea>
+            <textarea name="txt58" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt58']; ?></textarea>
             <div class="subgrid">
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
-                <textarea name="" id="" cols="10" rows="2"></textarea>
+                <textarea name="txt59" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt59']; ?></textarea>
+                <textarea name="txt60" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt60']; ?></textarea>
+                <textarea name="txt61" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt61']; ?></textarea>
+                <textarea name="txt62" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt62']; ?></textarea>
+                <textarea name="txt63" id="" cols="10" rows="2"><?php if (!empty($linha)) echo $linha['txt63']; ?></textarea>
             </div>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
+            <textarea name="txt64" id="" cols="30" rows="5"><?php if (!empty($linha)) echo $linha['txt64']; ?></textarea>
 
         </div>
         <input id="botao-enviar" type="submit" value="ENVIAR RESULTADOS" style="height: 30px; margin-left: 15px;">
---
+++ plano-de-acao-5w2h/sql.php
@@ -0,0 +1,87 @@
+<?php
+include_once "../includes/connection.php";
+
+$usuario_id = $_REQUEST['usuario_id'];
+$objetivo = $_REQUEST['objetivo'];
+$consultor = $_REQUEST['consultor'];
+$setor = $_REQUEST['setor'];
+$elaboracao = $_REQUEST['elaboracao'];
+$atualizacao = $_REQUEST['atualizacao'];
+$txt1 = $_REQUEST['txt1'];
+$txt2 = $_REQUEST['txt2'];
+$txt3 = $_REQUEST['txt3'];
+$txt4 = $_REQUEST['txt4'];
+$txt5 = $_REQUEST['txt5'];
+$txt6 = $_REQUEST['txt6'];
+$txt7 = $_REQUEST['txt7'];
+$txt8 = $_REQUEST['txt8'];
+$txt9 = $_REQUEST['txt9'];
+$txt10 = $_REQUEST['txt10'];
+$txt11 = $_REQUEST['txt11'];
+$txt12 = $_REQUEST['txt12'];
+$txt13 = $_REQUEST['txt13'];
+$txt14 = $_REQUEST['txt14'];
+$txt15 = $_REQUEST['txt15'];
+$txt16 = $_REQUEST['txt16'];
+$txt17 = $_REQUEST['txt17'];
+$txt18 = $_REQUEST['txt18'];
+$txt19 = $_REQUEST['txt19'];
+$txt20 = $_REQUEST['txt20'];
+$txt21 = $_REQUEST['txt21'];
+$txt22 = $_REQUEST['txt22'];
+$txt23 = $_REQUEST['txt23'];
+$txt24 = $_REQUEST['txt24'];
+$txt25 = $_REQUEST['txt25'];
+$txt26 = $_REQUEST['txt26'];
+$txt27 = $_REQUEST['txt27'];
+$txt28 = $_REQUEST['txt28'];
+$txt29 = $_REQUEST['txt29'];
+$txt30 = $_REQUEST['txt30'];
+$txt31 = $_REQUEST['txt31'];
+$txt32 = $_REQUEST['txt32'];
+$txt33 = $_REQUEST['txt33'];
+$txt34 = $_REQUEST['txt34'];
+$txt35 = $_REQUEST['txt35'];
+$txt36 = $_REQUEST['txt36'];
+$txt37 = $_REQUEST['txt37'];
+$txt38 = $_REQUEST['txt38'];
+$txt39 = $_REQUEST['txt39'];
+$txt40 = $_REQUEST['txt40'];
+$txt41 = $_REQUEST['txt41'];
+$txt42 = $_REQUEST['txt42'];
+$txt43 = $_REQUEST['txt43'];
+$txt44 = $_REQUEST['txt44'];
+$txt45 = $_REQUEST['txt45'];
+$txt46 = $_REQUEST['txt46'];
+$txt47 = $_REQUEST['txt47'];
+$txt48 = $_REQUEST['txt48'];
+$txt49 = $_REQUEST['txt49'];
+$txt50 = $_REQUEST['txt50'];
+$txt51 = $_REQUEST['txt51'];
+$txt52 = $_REQUEST['txt52'];
+$txt53 = $_REQUEST['txt53'];
+$txt54 = $_REQUEST['txt54'];
+$txt55 = $_REQUEST['txt55'];
+$txt56 = $_REQUEST['txt56'];
+$txt57 = $_REQUEST['txt57'];
+$txt58 = $_REQUEST['txt58'];
+$txt59 = $_REQUEST['txt59'];
+$txt60 = $_REQUEST['txt60'];
+$txt61 = $_REQUEST['txt61'];
+$txt62 = $_REQUEST['txt62'];
+$txt63 = $_REQUEST['txt63'];
+$txt64 = $_REQUEST['txt64'];
+
+$data = $conn->query('SELECT * FROM plano_de_acao_5w2h');
+$linha = mysqli_fetch_assoc($data);
+if (!empty($linha) && $linha['usuario_id'] == $usuario_id) {
+    $sql = "UPDATE plano_de_acao_5w2h set usuario_id='$usuario_id', objetivo='$objetivo', consultor='$consultor', setor='$setor', elaboracao='$elaboracao', atualizacao='$atualizacao', txt1='$txt1', txt2='$txt2', txt3='$txt3', txt4='$txt4', txt5='$txt5', txt6='$txt6', txt7='$txt7', txt8='$txt8', txt9='$txt9', txt10='$txt10', txt11='$txt11', txt12='$txt12', txt13='$txt13', txt14='$txt14', txt15='$txt15', txt16='$txt16', txt17='$txt17', txt18='$txt18', txt19='$txt19', txt20='$txt20', txt21='$txt21', txt22='$txt22', txt23='$txt23', txt24='$txt24', txt25='$txt25', txt26='$txt26', txt27='$txt27', txt28='$txt28', txt29='$txt29', txt30='$txt30', txt31='$txt31', txt32='$txt32', txt33='$txt33', txt34='$txt34', txt35='$txt35', txt36='$txt36', txt37='$txt37', txt38='$txt38', txt39='$txt39', txt40='$txt40', txt41='$txt41', txt42='$txt42', txt43='$txt43', txt44='$txt44', txt45='$txt45', txt46='$txt46', txt47='$txt47', txt48='$txt48', txt49='$txt49', txt50='$txt50', txt51='$txt51', txt52='$txt52', txt53='$txt53', txt54='$txt54', txt55='$txt55', txt56='$txt56', txt57='$txt57', txt58='$txt58', txt59='$txt59', txt60='$txt60', txt61='$txt61', txt62='$txt62', txt63='$txt63', txt64='$txt64' WHERE usuario_id= $usuario_id";
+} else {
+    $sql = "INSERT INTO plano_de_acao_5w2h set usuario_id='$usuario_id', objetivo='$objetivo', consultor='$consultor', setor='$setor', elaboracao='$elaboracao', atualizacao='$atualizacao', txt1='$txt1', txt2='$txt2', txt3='$txt3', txt4='$txt4', txt5='$txt5', txt6='$txt6', txt7='$txt7', txt8='$txt8', txt9='$txt9', txt10='$txt10', txt11='$txt11', txt12='$txt12', txt13='$txt13', txt14='$txt14', txt15='$txt15', txt16='$txt16', txt17='$txt17', txt18='$txt18', txt19='$txt19', txt20='$txt20', txt21='$txt21', txt22='$txt22', txt23='$txt23', txt24='$txt24', txt25='$txt25', txt26='$txt26', txt27='$txt27', txt28='$txt28', txt29='$txt29', txt30='$txt30', txt31='$txt31', txt32='$txt32', txt33='$txt33', txt34='$txt34', txt35='$txt35', txt36='$txt36', txt37='$txt37', txt38='$txt38', txt39='$txt39', txt40='$txt40', txt41='$txt41', txt42='$txt42', txt43='$txt43', txt44='$txt44', txt45='$txt45', txt46='$txt46', txt47='$txt47', txt48='$txt48', txt49='$txt49', txt50='$txt50', txt51='$txt51', txt52='$txt52', txt53='$txt53', txt54='$txt54', txt55='$txt55', txt56='$txt56', txt57='$txt57', txt58='$txt58', txt59='$txt59', txt60='$txt60', txt61='$txt61', txt62='$txt62', txt63='$txt63', txt64='$txt64'";
+}
+if (mysqli_query($conn, $sql)) {
+    echo "RESPOSTA GRAVADA COM SUCESSO!";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
