e599ae15db5f89367e847696a8997283cd276801
lucasmatutani <lucasmatutani@gmail.com>
2022-12-09 16:02:31 -0300

    hotfix

--- teste-aida/respostas/index.php
+++ teste-aida/respostas/index.php
@@ -43,10 +43,10 @@ if (!empty($data)) {
             <h1 class="h1-equipe" id="equipe"></h1>
         </div>
         <div class="container-prod">
-            <h2 id="prod1" onclick="prod1()"><?php if (!empty($linha)) echo $linha['nm1']; ?></h2>
-            <h2 id="prod2" onclick="prod2()"><?php if (!empty($linha)) echo $linha['nm2']; ?></h2>
-            <h2 id="prod3" onclick="prod3()"><?php if (!empty($linha)) echo $linha['nm3']; ?></h2>
-            <h2 id="prod4" onclick="prod4()"><?php if (!empty($linha)) echo $linha['nm4']; ?></h2>
+            <h2 id="prod1" onclick="prod1()"></h2>
+            <h2 id="prod2" onclick="prod2()"></h2>
+            <h2 id="prod3" onclick="prod3()"></h2>
+            <h2 id="prod4" onclick="prod4()"></h2>
         </div>
 
         <hr color="#000" class="border">
@@ -55,7 +55,7 @@ if (!empty($data)) {
 
             <div class="container-atencao">
                 <label for="">ATENÇÃO</label>
-                <textarea readonly name="atencao" id="txt1" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt1']; ?></textarea>
+                <textarea readonly name="atencao" id="txt1" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-atencao1" id="">
@@ -69,7 +69,7 @@ if (!empty($data)) {
 
             <div class="container-interesse">
                 <label for="">INTERESSE</label>
-                <textarea readonly name="interesse" id="txt2" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt2']; ?></textarea>
+                <textarea readonly name="interesse" id="txt2" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-interesse1" id="">
@@ -82,7 +82,7 @@ if (!empty($data)) {
 
             <div class="container-desejo">
                 <label for="">DESEJO</label>
-                <textarea readonly name="desejo" id="txt3" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt3']; ?></textarea>
+                <textarea readonly name="desejo" id="txt3" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-desejo1" id="">
@@ -95,7 +95,7 @@ if (!empty($data)) {
 
             <div class="container-acao">
                 <label for="">AÇÃO</label>
-                <textarea readonly name="acao" id="txt4" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt4']; ?></textarea>
+                <textarea readonly name="acao" id="txt4" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-acao1" id="">
@@ -108,7 +108,7 @@ if (!empty($data)) {
 
             <div class="container-feedback">
                 <label for="">FEEDBACK</label>
-                <textarea name="feedback1" id="feedback1" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['feedback1']; ?></textarea>
+                <textarea name="feedback1" id="feedback1" cols="130" rows="10"></textarea>
             </div>
 
 
@@ -118,7 +118,7 @@ if (!empty($data)) {
 
             <div class="container-atencao">
                 <label for="">ATENÇÃO</label>
-                <textarea readonly name="atencao2" id="txt5" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt5']; ?></textarea>
+                <textarea readonly name="atencao2" id="txt5" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-atencao2" id="">
@@ -132,7 +132,7 @@ if (!empty($data)) {
 
             <div class="container-interesse">
                 <label for="">INTERESSE</label>
-                <textarea readonly name="interesse" id="txt6" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt6']; ?></textarea>
+                <textarea readonly name="interesse" id="txt6" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-interesse2" id="">
@@ -145,7 +145,7 @@ if (!empty($data)) {
 
             <div class="container-desejo">
                 <label for="">DESEJO</label>
-                <textarea readonly name="desejo" id="txt7" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt7']; ?></textarea>
+                <textarea readonly name="desejo" id="txt7" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-desejo2" id="">
@@ -158,7 +158,7 @@ if (!empty($data)) {
 
             <div class="container-acao">
                 <label for="">AÇÃO</label>
-                <textarea readonly name="acao" id="txt8" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt8']; ?></textarea>
+                <textarea readonly name="acao" id="txt8" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-acao2" id="">
@@ -171,7 +171,7 @@ if (!empty($data)) {
 
             <div class="container-feedback">
                 <label for="">FEEDBACK</label>
-                <textarea name="feedback2" id="feedback2" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['feedback2']; ?></textarea>
+                <textarea name="feedback2" id="feedback2" cols="130" rows="10"></textarea>
             </div>
 
 
@@ -181,7 +181,7 @@ if (!empty($data)) {
 
             <div class="container-atencao">
                 <label for="">ATENÇÃO</label>
-                <textarea readonly name="atencao" id="txt9" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt9']; ?></textarea>
+                <textarea readonly name="atencao" id="txt9" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-atencao3" id="">
@@ -195,7 +195,7 @@ if (!empty($data)) {
 
             <div class="container-interesse">
                 <label for="">INTERESSE</label>
-                <textarea readonly name="interesse" id="txt10" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt10']; ?></textarea>
+                <textarea readonly name="interesse" id="txt10" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-interesse3" id="">
@@ -208,7 +208,7 @@ if (!empty($data)) {
 
             <div class="container-desejo">
                 <label for="">DESEJO</label>
-                <textarea readonly name="desejo" id="txt11" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt11']; ?></textarea>
+                <textarea readonly name="desejo" id="txt11" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-desejo3" id="">
@@ -221,7 +221,7 @@ if (!empty($data)) {
 
             <div class="container-acao">
                 <label for="">AÇÃO</label>
-                <textarea readonly name="acao" id="txt12" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt12']; ?></textarea>
+                <textarea readonly name="acao" id="txt12" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-acao3" id="">
@@ -234,7 +234,7 @@ if (!empty($data)) {
 
             <div class="container-feedback">
                 <label for="">FEEDBACK</label>
-                <textarea name="feedback3" id="feedback3" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['feedback3']; ?></textarea>
+                <textarea name="feedback3" id="feedback3" cols="130" rows="10"></textarea>
             </div>
 
         </div>
@@ -243,7 +243,7 @@ if (!empty($data)) {
 
             <div class="container-atencao">
                 <label for="">ATENÇÃO</label>
-                <textarea readonly name="atencao" id="txt13" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt13']; ?></textarea>
+                <textarea readonly name="atencao" id="txt13" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-atencao4" id="">
@@ -257,7 +257,7 @@ if (!empty($data)) {
 
             <div class="container-interesse">
                 <label for="">INTERESSE</label>
-                <textarea readonly name="interesse" id="txt14" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt14']; ?></textarea>
+                <textarea readonly name="interesse" id="txt14" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-interesse4" id="">
@@ -270,7 +270,7 @@ if (!empty($data)) {
 
             <div class="container-desejo">
                 <label for="">DESEJO</label>
-                <textarea readonly name="desejo" id="txt15" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt15']; ?></textarea>
+                <textarea readonly name="desejo" id="txt15" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-desejo4" id="">
@@ -283,7 +283,7 @@ if (!empty($data)) {
 
             <div class="container-acao">
                 <label for="">AÇÃO</label>
-                <textarea readonly name="acao" id="txt16" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['txt16']; ?></textarea>
+                <textarea readonly name="acao" id="txt16" cols="130" rows="10"></textarea>
             </div>
             <div class="btns-radios">
                 <input value="1" type="radio" name="radio-acao4" id="">
@@ -296,7 +296,7 @@ if (!empty($data)) {
 
             <div class="container-feedback">
                 <label for="">FEEDBACK</label>
-                <textarea name="feedback4" id="feedback4" cols="130" rows="10"><?php if (!empty($linha)) echo $linha['feedback4']; ?></textarea>
+                <textarea name="feedback4" id="feedback4" cols="130" rows="10"></textarea>
             </div>
 
         </div>
@@ -313,33 +313,33 @@ if (!empty($data)) {
 </html>
 
 <script>
-    // arrGrupo = $.parseJSON(localStorage.getItem("grupo_escolhido"));
-    // document.getElementById('user_id').value = arrGrupo["id"];
-    // $("#equipe").html(arrGrupo["grupo"]);
-    // $("#prod1").html(arrGrupo["nm1"]);
-    // $("#prod2").html(arrGrupo["nm2"]);
-    // $("#prod3").html(arrGrupo["nm3"]);
-    // $("#prod4").html(arrGrupo["nm4"]);
-    // $("#txt1").html(arrGrupo["txt1"]);
-    // $("#txt2").html(arrGrupo["txt2"]);
-    // $("#txt3").html(arrGrupo["txt3"]);
-    // $("#txt4").html(arrGrupo["txt4"]);
-    // $("#txt5").html(arrGrupo["txt5"]);
-    // $("#txt6").html(arrGrupo["txt6"]);
-    // $("#txt7").html(arrGrupo["txt7"]);
-    // $("#txt8").html(arrGrupo["txt8"]);
-    // $("#txt9").html(arrGrupo["txt9"]);
-    // $("#txt10").html(arrGrupo["txt10"]);
-    // $("#txt11").html(arrGrupo["txt11"]);
-    // $("#txt12").html(arrGrupo["txt12"]);
-    // $("#txt13").html(arrGrupo["txt13"]);
-    // $("#txt14").html(arrGrupo["txt14"]);
-    // $("#txt15").html(arrGrupo["txt15"]);
-    // $("#txt16").html(arrGrupo["txt16"]);
-    // $("#feedback1").html(arrGrupo['feedback1']);
-    // $("#feedback2").html(arrGrupo['feedback2']);
-    // $("#feedback3").html(arrGrupo['feedback3']);
-    // $("#feedback4").html(arrGrupo['feedback4']);
+    arrGrupo = $.parseJSON(localStorage.getItem("grupo_escolhido"));
+    document.getElementById('user_id').value = arrGrupo["id"];
+    $("#equipe").html(arrGrupo["grupo"]);
+    $("#prod1").html(arrGrupo["nm1"]);
+    $("#prod2").html(arrGrupo["nm2"]);
+    $("#prod3").html(arrGrupo["nm3"]);
+    $("#prod4").html(arrGrupo["nm4"]);
+    $("#txt1").html(arrGrupo["txt1"]);
+    $("#txt2").html(arrGrupo["txt2"]);
+    $("#txt3").html(arrGrupo["txt3"]);
+    $("#txt4").html(arrGrupo["txt4"]);
+    $("#txt5").html(arrGrupo["txt5"]);
+    $("#txt6").html(arrGrupo["txt6"]);
+    $("#txt7").html(arrGrupo["txt7"]);
+    $("#txt8").html(arrGrupo["txt8"]);
+    $("#txt9").html(arrGrupo["txt9"]);
+    $("#txt10").html(arrGrupo["txt10"]);
+    $("#txt11").html(arrGrupo["txt11"]);
+    $("#txt12").html(arrGrupo["txt12"]);
+    $("#txt13").html(arrGrupo["txt13"]);
+    $("#txt14").html(arrGrupo["txt14"]);
+    $("#txt15").html(arrGrupo["txt15"]);
+    $("#txt16").html(arrGrupo["txt16"]);
+    $("#feedback1").html(arrGrupo['feedback1']);
+    $("#feedback2").html(arrGrupo['feedback2']);
+    $("#feedback3").html(arrGrupo['feedback3']);
+    $("#feedback4").html(arrGrupo['feedback4']);
 
     var input = document.getElementsByTagName('input');
     arrRadioTotal = arrGrupo["radio_total"].split("");
