bd14fdd37382c19bf086428bbf7b49756c984725
lucasmatutani <lucasmatutani@gmail.com>
2023-01-14 14:06:52 -0300

    hotfix: adicionando o banco

--- testes-jpb/cenario.php
+++ testes-jpb/cenario.php
@@ -10,9 +10,9 @@ $perg2 = $_REQUEST['perg2'];
 $data = $conn->query('SELECT * FROM resposta_cenario');
 $linha = mysqli_fetch_assoc($data);
 if (!empty($linha) && $linha['usuario_id'] == $usuario_id) {
-    $sql = "UPDATE respota_cenario set email='$email', perg1='$perg1', perg2='$perg2' WHERE usuario_id= $usuario_id";
+    $sql = "UPDATE resposta_cenario set email='$email', perg1='$perg1', perg2='$perg2' WHERE usuario_id = $usuario_id";
 } else {
-    $sql = "INSERT INTO respota_cenario set email='$email', perg1='$perg1', perg2='$perg2'";
+    $sql = "INSERT INTO resposta_cenario set email='$email', perg1='$perg1', perg2='$perg2', usuario_id = $usuario_id";
 }
 if (mysqli_query($conn, $sql)) {
     header("location: ../testes");
