1fe677b50de9113a5abcee798706a25a52713331
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2023-01-15 23:33:00 -0300

    alteração layout matris de risco

--- teste-matriz-de-risco/index.php
+++ teste-matriz-de-risco/index.php
@@ -33,105 +33,65 @@ if (!empty($data)) {
         <h1>Matriz de Risco</h1>
     </div>
     <form action="./sql.php" method="POST">
-        <h2 class="probabilidade">Probabilidade</h2>
 
-        <div class=" container-principal">
-            <div></div>
-            <div>
-                <h3>Risco Médio</h3>
-            </div>
-            <div>
-                <h3>Risco Alto</h3>
-            </div>
-            <div>
+        <div class="container-flex">
+            <h2 class="probabilidade">Probabilidade</h2>
 
-                <h3>Risco Máximo</h3>
-            </div>
+            <div class=" container-principal">
+                <div class="titulo">
+                    <h3>Alto</h3>
+                    <h3>Médio</h3>
+                    <h3>Baixo</h3>
+                </div>
+                <div class="incio">
+                    <h3>Risco Médio</h3>
+                    <input style="border: 3px solid yellow" type=" number" name="input1" id="" value="<?php if (!empty($linha)) echo $linha['input1']; ?>">
+                    <h3>Risco Baixo</h3>
+                    <input type="number" name="input5" id="" value=" <?php if (!empty($linha)) echo $linha['input5']; ?>">
+                    <h3>Risco Mínimo</h3>
+                    <input type="number" name="input7" id="" value=" <?php if (!empty($linha)) echo $linha['input7']; ?>">
+                    <h3>Baixa</h3>
+                </div>
+                <div class="inicio">
+                    <h3>Risco Alto</h3>
+                    <input style="border: 3px solid pink" type="number" name="input2" id="" value="<?php if (!empty($linha)) echo $linha['input2']; ?>">
+                    <h3>Risco Médio</h3>
+                    <input style="border: 3px solid yellow" type="number" name="input6" id="" value=" <?php if (!empty($linha)) echo $linha['input6']; ?>">
+                    <h3>Risco Baixo</h3>
+                    <input type="number" name="input8" id="" value=" <?php if (!empty($linha)) echo $linha['input8']; ?>">
+                    <h3>Médio</h3>
+                    <h2 class="impactos">Impacto</h2>
 
-            <div>
-                <h3>Baixa</h3>
+                </div>
+                <div class="inicio">
+                    <h3>Risco Máximo</h3>
+                    <input style="border: 3px solid red" type="number" name="input3" id="" value="<?php if (!empty($linha)) echo $linha['input3']; ?>">
+                    <h3>Risco Alto</h3>
+                    <input style="border: 3px solid pink" type="number" name="input4" id="" value=" <?php if (!empty($linha)) echo $linha['input4']; ?>">
+                    <h3>Risco Médio</h3>
+                    <input style="border: 3px solid yellow" type="number" name="input9" id="" value=" <?php if (!empty($linha)) echo $linha['input9']; ?>">
+                    <h3>Alto</h3>
+                </div>
             </div>
-            <div>
-                <input type="number" name="input1" id="" value="<?php if (!empty($linha)) echo $linha['input1']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input2" id="" value="<?php if (!empty($linha)) echo $linha['input2']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input3" id="" value="<?php if (!empty($linha)) echo $linha['input3']; ?>">
-            </div>
-            <div></div>
-            <div>
-                <h3>Risco Baixo</h3>
-            </div>
-            <div>
-                <h3>Risco Médio</h3>
-            </div>
-            <div>
-                <h3>Risco Alto</h3>
-            </div>
-            <div>
-                <h3>Médio</h3>
-            </div>
-            <div>
-                <input type="number" name="input4" id="" value=" <?php if (!empty($linha)) echo $linha['input4']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input5" id="" value=" <?php if (!empty($linha)) echo $linha['input5']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input6" id="" value=" <?php if (!empty($linha)) echo $linha['input6']; ?>">
-            </div>
-            <div></div>
-            <div>
-                <h3>Risco Mínimo</h3>
-            </div>
-            <div>
-                <h3>Risco Baixo</h3>
-            </div>
-            <div>
-                <h3>Risco Médio</h3>
-            </div>
-            <div>
-                <h3>Alto</h3>
-            </div>
-            <div>
-                <input type="number" name="input7" id="" value=" <?php if (!empty($linha)) echo $linha['input7']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input8" id="" value=" <?php if (!empty($linha)) echo $linha['input8']; ?>">
-            </div>
-            <div>
-                <input type="number" name="input9" id="" value=" <?php if (!empty($linha)) echo $linha['input9']; ?>">
-            </div>
-            <div></div>
-            <div class="impacto">
-                <h3>Baixa</h3>
-            </div>
-            <div>
-                <h3>Médio</h3>
-            </div>
-            <div>
-                <h3>Alto</h3>
+            <div class="container-riscos">
+                <h2>Riscos</h2>
+                <label>1</label><textarea name="txt1" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt1']; ?></textarea>
+                <label>2</label><textarea name="txt2" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt2']; ?></textarea>
+                <label>3</label><textarea name="txt3" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt3']; ?></textarea>
+                <label>4</label><textarea name="txt4" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt4']; ?></textarea>
+                <label>5</label><textarea name="txt5" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt5']; ?></textarea>
+                <label>6</label><textarea name="txt6" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt6']; ?></textarea>
+                <label>7</label><textarea name="txt7" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt7']; ?></textarea>
+                <label>8</label><textarea name="txt8" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt8']; ?></textarea>
+                <label>9</label><textarea name="txt9" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt9']; ?></textarea>
+                <label>10</label><textarea name="txt10" id="" cols="30" rows="4"><?php if (!empty($linha)) echo $linha['txt10']; ?></textarea>
             </div>
         </div>
 
 
-        <!-- <i class="fa fa-long-arrow-right" aria-hidden="true" style="width: 100px;"></i> -->
-        <h2 class="impactos">Impacto</h2>
-        <div class="container-riscos">
-            <h2>Riscos</h2>
-            <h4>1</h4><textarea name="txt1" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt1']; ?></textarea>
-            <h4>2</h4><textarea name="txt2" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt2']; ?></textarea>
-            <h4>3</h4><textarea name="txt3" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt3']; ?></textarea>
-            <h4>4</h4><textarea name="txt4" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt4']; ?></textarea>
-            <h4>5</h4><textarea name="txt5" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt5']; ?></textarea>
-            <h4>6</h4><textarea name="txt6" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt6']; ?></textarea>
-            <h4>7</h4><textarea name="txt7" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt7']; ?></textarea>
-            <h4>8</h4><textarea name="txt8" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt8']; ?></textarea>
-            <h4>9</h4><textarea name="txt9" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt9']; ?></textarea>
-            <h4>10</h4><textarea name="txt10" id="" cols="30" rows="2"><?php if (!empty($linha)) echo $linha['txt10']; ?></textarea>
-        </div>
+
+
+
 
         <input id="botao-enviar" type="submit" value="ENVIAR RESULTADOS" style="height: 30px; margin-left: 15px;">
     </form>
