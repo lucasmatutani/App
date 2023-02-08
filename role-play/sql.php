c84a7ef8ea200117689714a22e12a4e97f64887b
lucasmatutani <lucasmatutani@gmail.com>
2023-02-08 20:59:31 -0300

    hotfix

--- role-play/sql.php
+++ role-play/sql.php
@@ -5,15 +5,15 @@ $grupo = $_REQUEST['grupo'];
 $participante = $_REQUEST['participante'];
 $rodada = $_REQUEST['rodada'];
 $representante = $_REQUEST['representante'];
-$cp1 = $_REQUEST['cp'];
-$rap1 = $_REQUEST['rap'];
-$abe1 = $_REQUEST['abe'];
-$ad1 = $_REQUEST['ad'];
-$neg1 = $_REQUEST['neg'];
-$fc1 = $_REQUEST['fc'];
-$rv1 = $_REQUEST['rv'];
-$opv1 = $_REQUEST['opv'];
-$ct1 = $_REQUEST['ct'];
+$cp = $_REQUEST['cp'];
+$rap = $_REQUEST['rap'];
+$abe = $_REQUEST['abe'];
+$ad = $_REQUEST['ad'];
+$neg = $_REQUEST['neg'];
+$fc = $_REQUEST['fc'];
+$rv = $_REQUEST['rv'];
+$opv = $_REQUEST['opv'];
+$ct = $_REQUEST['ct'];
 
 $sql = "INSERT INTO role_play ( grupo, participante ,rodada, representante, cp, ap, rap, abe, ad, neg, fc, rv, opv, ct ) VALUES ('$grupo', '$participante',  '$rodada', '$representante', '$cp', '$ap', '$rap', '$abe', '$ad', '$neg', '$fc', '$rv', '$opv', '$ct' )";
 
