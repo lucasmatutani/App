f171afa728bfdd99c9324b78bde34e1efacb9373
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2023-02-10 16:09:14 -0300

    alteração table respostas role play

--- role-play/respostas/index.php
+++ role-play/respostas/index.php
@@ -11,80 +11,41 @@
 
 <body>
     <a class="btn-voltar" href="http://focustradeapp.com.br/testes/"><i style="margin-right: 5px;" class="fa-solid fa-caret-left"></i> <strong> Voltar</a>
+    <div></div>
     <div class="respostas">
         <h1>Respostas</h1>
     </div>
 
 
     <!-- as td's vazias são para o espaçamento -->
-    <table cellpadding="20" border="1px solid #000">
-        <tr>
-            <td></td>
-            <td></td>
-            <td>
-                <h2>Nota</h2>
-            </td>
-        </tr>
-        <tr>
-            <th>Planejamento</th>
-            <td>
-                <p>Apresentação do Planejamento para os Avaliadores</p>
-            </td>
-            <td></td>
-        </tr>
 
-        <tr>
-            <th>
-                Prospecção
-            </th>
-            <td>
-                <p>Rapport/conexão com o cliente</p>
-            </td>
-            <td></td>
+    <div class="container-principal">
+        <div class="table1">
+            <table cellpadding="40" cellspacing="20">
+                <tr>
+                    <td>Código do Participante</td>
+                    <td>Grupo</td>
+                    <td>Avaliado</td>
+                    <td>Total</td>
+                </tr>
 
-        </tr>
+            </table>
+        </div>
 
-        <tr>
-            <td></td>
-            <td>
-                <p>Apresentação e Desenvolvimento</p>
-            </td>
-            <td></td>
-        </tr>
-
-        <tr>
-            <th>
-                Pedido
-            </th>
-            <td>
-                <p>Falteiro/Demanda</p>
-            </td>
-            <td></td>
-        <tr>
-            <th>
-                <p>Negociação</p>
-            </th>
-            <td>
-                Busca do Objetivo
-            </td>
-            <td></td>
-
-        </tr>
-        </tr>
-
-        <tr>
-            <th>Outros</th>
-            <td>Técnica de Fechamennto Aplicada</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <p>Cumpriu com o Tempo</p>
-            </td>
-            <td></td>
-        </tr>
-    </table>
+        <div class="table2">
+            <table cellpadding="30" cellspacing="20">
+                <td>Total dos Grupos</td>
+                <tr>
+                    <td>Grupo 1</td>
+                    <td>Grupo 2</td>
+                    <td>Grupo 3</td>
+                    <td>Grupo 4</td>
+                    <td>Grupo 5</td>
+                    <td>Grupo 6</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
