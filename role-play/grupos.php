d72c5e3a75331801a5337548071a50007377af49
lucasmatutani <lucasmatutani@gmail.com>
2023-02-11 13:58:20 -0300

    hotfix

--- role-play/grupos.php
+++ role-play/grupos.php
@@ -15,11 +15,11 @@
     <div class="container-principal">
         <h1>Grupos</h1>
         <a href="./avaliacao-grupo/index.php?grupo=1">Grupo 1</a>
-        <a href="www.focustradeapp.com.br/role-play/avaliacao-grupo?grupo=2">Grupo 2</a>
-        <a href="focustradeapp.com.br/role-play/avaliacao-grupo?grupo=3">Grupo 3</a>
-        <a href="focustradeapp.com.br/role-play/avaliacao-grupo?grupo=4">Grupo 4</a>
-        <a href="focustradeapp.com.br/role-play/avaliacao-grupo?grupo=5">Grupo 5</a>
-        <a href="focustradeapp.com.br/role-play/avaliacao-grupo?grupo=6">Grupo 6</a>
+        <a href="./avaliacao-grupo/index.php?grupo=2">Grupo 2</a>
+        <a href="./avaliacao-grupo/index.php?grupo=3">Grupo 3</a>
+        <a href="./avaliacao-grupo/index.php?grupo=4">Grupo 4</a>
+        <a href="./avaliacao-grupo/index.php?grupo=5">Grupo 5</a>
+        <a href="./avaliacao-grupo/index.php?grupo=6">Grupo 6</a>
     </div>
 </body>
 
