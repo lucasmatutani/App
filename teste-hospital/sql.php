b62b0a021df27fa92bc90b42adc285720d319d81
lucasmatutani <lucasmatutani@gmail.com>
2023-04-03 00:34:21 -0300

    hotfix

--- teste-hospital/sql.php
+++ teste-hospital/sql.php
@@ -12,9 +12,9 @@ $objetivo = $_REQUEST['objetivo'];
 $data = $conn->query("SELECT * FROM diagnostico WHERE usuario_id = $usuario_id");
 $linha = mysqli_fetch_assoc($data);
 if (!empty($linha)) {
-    $sql = "UPDATE diagnostico set usuario_id='$usuario_id', representante='$representante', nome='$nome', problema='$problemas', oportunidade='$oportunidade', objetivo='$objetivo' WHERE usuario_id= $usuario_id";
+    $sql = "UPDATE diagnostico set usuario_id='$usuario_id', representante='$representante', nome='$nome', problemas='$problemas', oportunidade='$oportunidade', objetivo='$objetivo' WHERE usuario_id= $usuario_id";
 } else {
-    $sql = "INSERT INTO diagnostico set usuario_id= '$usuario_id', representante='$representante', nome='$nome', problema='$problemas', oportunidade='$oportunidade', objetivo='$objetivo'";
+    $sql = "INSERT INTO diagnostico set usuario_id= '$usuario_id', representante='$representante', nome='$nome', problemas='$problemas', oportunidade='$oportunidade', objetivo='$objetivo'";
 }
 
 if (mysqli_query($conn, $sql)) {
