192d4c12f08ecb38eba4ec9f94b504cc8d407541
FabricioFavero <107579459+FabricioFavero@users.noreply.github.com>
2023-04-26 11:54:34 -0300

    alteração sql teste flowchart

--- teste-flowchart/sql.php
+++ teste-flowchart/sql.php
@@ -14,12 +14,12 @@ $alta2 = $_REQUEST['alta2'];
 $alta3 = $_REQUEST['alta3'];
 
 
-$data = $conn->query("SELECT * FROM  WHERE usuario_id = $usuario_id");
+$data = $conn->query("SELECT * FROM teste_flowchart WHERE usuario_id = $usuario_id");
 $linha = mysqli_fetch_assoc($data);
 if (!empty($linha)) {
-    $sql = "UPDATE  set usuario_id='$usuario_id', email='$email', baixa1='$baixa1', baixa2='$baixa2', baixa3='$baixa3', media1='$media1', media2='$media2', media3='$media3', alta1='$alta1', alta2='$alta2', alta3='$alta3' WHERE usuario_id= $usuario_id";
+    $sql = "UPDATE teste_flowchart set usuario_id='$usuario_id', email='$email', baixa1='$baixa1', baixa2='$baixa2', baixa3='$baixa3', media1='$media1', media2='$media2', media3='$media3', alta1='$alta1', alta2='$alta2', alta3='$alta3' WHERE usuario_id= $usuario_id";
 } else {
-    $sql = "INSERT INTO  set usuario_id= '$usuario_id', representante='$representante', nome='$nome', grupo='$grupo', problemas='$problemas', oportunidade='$oportunidade', objetivo='$objetivo'";
+    $sql = "INSERT INTO teste_flowchart set usuario_id= '$usuario_id', representante='$representante', nome='$nome', grupo='$grupo', problemas='$problemas', oportunidade='$oportunidade', objetivo='$objetivo'";
 }
 
 if (mysqli_query($conn, $sql)) {
