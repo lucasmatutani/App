427b8d8d787458d0bb3f8793cbf92cc7935ecf7d
lucasmatutani <lucasmatutani@gmail.com>
2023-07-31 18:57:53 -0300

    hotfix

--- roda-competencias/index.php
+++ roda-competencias/index.php
@@ -1,79 +1,37 @@
 <!DOCTYPE html>
-<html>
-
-<head>
-    <title>Gráfico de Pizza Dinâmico em Círculo</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <style>
-        .chart-container {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 300px;
-            height: 300px;
-        }
-    </style>
-</head>
-
+<meta charset="utf-8">
 <body>
-    <div class="chart-container">
-        <canvas id="chart0" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart1" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart2" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart3" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart4" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart5" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart6" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart7" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart8" class="chart" width="100" height="100"></canvas>
-        <canvas id="chart9" class="chart" width="100" height="100"></canvas>
-    </div>
+<script src="https://d3js.org/d3.v5.min.js"></script>
+<script>
+var data = [1,1,1,1,1,1,1,1,1,1,1]; // 11 segmentos com tamanhos iguais
+var width = 420,
+    height = 420,
+    radius = Math.min(width, height) / 2;
 
-    <script>
-        var charts = [];
+var color = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#314b7c", "#aabbcc", "#ddaaff", "#334488"]); // cores para cada segmento
 
-        // Crie um gráfico de pizza para cada parte do círculo
-        for (var i = 0; i < 10; i++) {
-            var canvas = document.getElementById('chart' + i);
-            var angle = i * Math.PI / 5;
-            var x = Math.cos(angle) * 100 + 150;
-            var y = Math.sin(angle) * 100 + 150;
-            var chart = new Chart(canvas, {
-                type: 'pie',
-                data: {
-                    labels: ['', ''],
-                    datasets: [{
-                        data: [0, 10],
-                        backgroundColor: ['#FFFFFF', '#95A5A6']
-                    }]
-                },
-                options: {
-                    cutoutPercentage: 90,
-                    tooltips: {
-                        enabled: false
-                    },
-                    animation: {
-                        duration: 0
-                    },
-                    legend: {
-                        display: false
-                    }
-                }
-            });
-            canvas.style.left = x + 'px';
-            canvas.style.top = y + 'px';
-            charts.push(chart);
-        }
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
 
-        function updateChart() {
-            // Atualize cada gráfico com a pontuação da categoria correspondente
-            for (var i = 0; i < 10; i++) {
-                var categoryValue = document.getElementById('category' + i).value;
-                charts[i].data.datasets[0].data[0] = categoryValue;
-                charts[i].data.datasets[0].data[1] = 10 - categoryValue;
-                charts[i].update();
-            }
-        }
-    </script>
+var pie = d3.pie()
+    .sort(null);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var g = svg.selectAll(".arc")
+    .data(pie(data))
+  .enter().append("g")
+    .attr("class", "arc");
+
+g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data); });
+
+</script>
 </body>
