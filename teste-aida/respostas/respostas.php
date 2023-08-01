9a07019eeb3766e28ea70ee8764a0521f5a4f3b5
lucasmatutani <lucasmatutani@gmail.com>
2023-08-01 15:44:05 -0300

    hotfix

--- teste-aida/respostas/respostas.php
+++ teste-aida/respostas/respostas.php
@@ -10,6 +10,8 @@ $feedback3 = $_REQUEST['feedback3'];
 $feedback4 = $_REQUEST['feedback4'];
 
 $data = $conn->query("SELECT * FROM teste_aida WHERE usuario_id = $usuario_id");
+echo $data;
+exit();
 if (!empty($data)) {
     $sql = "UPDATE teste_aida SET feedback1 = '$feedback1', feedback2 = '$feedback2', feedback3 = '$feedback3', feedback4 = '$feedback4', valor_total = $valor_total, radio_total = '$radio_total' WHERE usuario_id = $usuario_id";
 }
